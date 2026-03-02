dca2ff9c725cd3882905e9817785c51e533d2c41
pqcuong737 <pqcuong737@gmail.com>
2026-03-03 01:16:22 +0700

    fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,10 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
+    base: '/',
     define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY),
+      'process.env.APP_URL': JSON.stringify(env.VITE_APP_URL),
     },
     resolve: {
       alias: {
